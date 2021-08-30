eb4d806dd81d2eaad88ee8e95cce636ef71f99ad
srson <srson@LAPTOP-4FF8CQ3I>
2021-08-30 13:55:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+
+        }
+    }
+}
